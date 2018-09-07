91dd7e80d61b99d82577f619bb6108e4e50be014
Ritvikjain <ritvikjain2@gmail.com>
2018-09-08 02:26:29 +0530

    firebase config added

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: false,
+    firebase:{
+        apiKey: "AIzaSyDZX7gSAci2f6UMWf63U4olqTgDSqs5FH8",
+        authDomain: "ionic-firebase-3f559.firebaseapp.com",
+        databaseURL: "https://ionic-firebase-3f559.firebaseio.com",
+        projectId: "ionic-firebase-3f559",
+        storageBucket: "ionic-firebase-3f559.appspot.com",
+        messagingSenderId: "863912870392"
+    }
+};
